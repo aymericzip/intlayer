51ceb7d6063efe0dd8a67ed67b4fa0b944cf147d
Aymeric PINEAU <ay.pineau@gmail.com>
2024-04-07 19:32:37 +0200

    wip

--- examples/nextjs-app/next.config.mjs
+++ examples/nextjs-app/next.config.mjs
@@ -1,7 +1,34 @@
 /** @type {import('next').NextConfig} */
-// import { webPackConfig } from 'intlayer-plugin';
-import withIntlayer from 'next-intlayer';
 
-const nextConfig = {};
+import { resolve } from 'path';
+import { withIntlayer } from 'next-intlayer';
 
-export default withIntlayer(nextConfig);
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      '@swc/core': '@swc/core',
+      vm: 'vm',
+      intlayer: 'intlayer',
+      'intlayer-react': 'intlayer-react',
+      'intlayer-config': 'intlayer-config',
+      'intlayer-webpack-app': 'intlayer-webpack-app',
+    });
+
+    // const relativeMainPath = relative(baseDirPath, mainDir);
+    // const relativePath = join(relativeMainPath, 'dictionaries.cjs');
+    const relativePath = './.intlayer/main/dictionaries.cjs';
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // your aliases
+      '@intlayer/dictionariesEntryPoint': relativePath,
+    };
+
+    console.log('config.resolve.alias', config.resolve.alias);
+
+    // Important: return the modified config
+    return config;
+  },
+};
+
+export default nextConfig;
