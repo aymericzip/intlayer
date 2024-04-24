a3f56d965c0faeb093e2f091c87a30a82fe0ce42
Aymeric PINEAU <ay.pineau@gmail.com>
2024-04-24 17:44:32 +0200

    refactor: restore example of craco.config.js in react-app

--- examples/react-app/craco.config.js
+++ examples/react-app/craco.config.js
@@ -1,10 +1,14 @@
-//
-// const intlayerPlugin = require('react-intlayer/craco-plugin');
+const intlayerPlugin = require('react-intlayer/craco-plugin');
 
-// module.exports = {
-//   plugins: [
-//     {
-//       plugin: intlayerPlugin,
-//     },
-//   ],
-// };
+/**
+ * Here an example of how to use the intlayer plugin in a custom craco configuration file.
+ *
+ * File not needed if you are using the 'react-intlayer' script in the package.json file to start and build the application.
+ */
+module.exports = {
+  plugins: [
+    {
+      plugin: intlayerPlugin,
+    },
+  ],
+};
