890a81b4bfdd4ff3c9d962b21d3cc68c3db7c2c8
Aymeric PINEAU <ay.pineau@gmail.com>
2024-05-14 23:43:15 +0200

    style(website): update landing layout

--- apps/website/src/components/LandingSection/index.tsx
+++ apps/website/src/components/LandingSection/index.tsx
@@ -20,13 +20,13 @@ export const LandingSection: FC = () => {
   const { title, description } = useIntlayer('landing-section');
 
   return (
-    <div className="flex size-full flex-col gap-16 p-20 md:w-full md:flex-row">
-      <div className="flex size-full flex-col items-center justify-center gap-6 md:w-1/2">
+    <div className="flex h-[150vh] w-full flex-col gap-16 p-20 md:size-full md:flex-row">
+      <div className="flex h-[100vh] w-full flex-col items-center justify-center gap-6 md:h-full md:w-3/5">
         <h1 className="text-4xl font-bold ">{title}</h1>
         <span className="text-neutral-500">{description}</span>
         <ActionButtons />
       </div>
-      <div className="flex w-full flex-col items-center justify-center gap-3 md:w-1/2">
+      <div className="flex w-full flex-col items-center justify-center gap-3 md:w-2/5">
         <div className="flex max-h-full w-full max-w-full">
           <IDE tabs={ideTabs} />
         </div>
