0382fa4178809a2633e0e17df8fa3b8083b6740d
Aymeric PINEAU <ay.pineau@gmail.com>
2024-05-15 12:41:16 +0200

    style(website): update action button style

--- apps/website/src/components/LandingPage/LandingSection/ActionButtons.tsx
+++ apps/website/src/components/LandingPage/LandingSection/ActionButtons.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from '@intlayer/design-system';
+import { AArrowDownIcon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useIntlayer } from 'next-intlayer';
 import type { FC } from 'react';
@@ -13,13 +14,14 @@ export const ActionButtons: FC = () => {
   } = useIntlayer('landing-section-action-button');
   const router = useRouter();
   return (
-    <div className="mt-10 flex w-full flex-row gap-4">
+    <div className="mt-10 flex w-full flex-col gap-4 sm:flex-row">
       <Button
         label={secondaryLabel}
         variant="outline"
         onClick={() => router.push(ExternalLinks.InterestOfIntlayer)}
         size="lg"
         color="text"
+        className="w-full md:w-auto"
       >
         {secondaryContent}
       </Button>
@@ -29,6 +31,7 @@ export const ActionButtons: FC = () => {
         variant="default"
         size="lg"
         color="text"
+        className="w-full md:w-auto"
       >
         {primaryContent}
       </Button>
