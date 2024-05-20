99c37e167e9ff1b0bb8a5a8f001f4e486f49d29d
Aymeric PINEAU <ay.pineau@gmail.com>
2024-05-20 15:08:09 -0600

    style(design-system): add gap footer

--- packages/@intlayer/design-system/src/components/Footer/index.tsx
+++ packages/@intlayer/design-system/src/components/Footer/index.tsx
@@ -14,12 +14,12 @@ export type LinkGroup = { title: string; links: Link[] };
 
 type FooterProps = { links?: LinkGroup[] };
 
-const StyledFooter = tw.footer`flex flex-auto flex-row justify-around p-6 items-center flex-wrap gap-4`;
+const StyledFooter = tw.footer`flex flex-auto flex-row justify-around p-6 items-center flex-wrap gap-10`;
 
 const StyledAsideContent = tw.aside`flex flex-col justify-between items-center md:w-1/4`;
 const StyledAsideDescription = tw.span`text-sm text-neutral text-center`;
 
-const StyledFooterContent = tw.div`w-full flex flex-row justify-around m-auto flex-wrap gap-4 md:w-2/3`;
+const StyledFooterContent = tw.div`w-full flex flex-row justify-around m-auto flex-wrap gap-2 md:w-2/3`;
 const StyledColumn = tw.div`flex flex-col gap-2`;
 const StyledColumnRow = tw.div`flex flex-col gap-3`;
 
