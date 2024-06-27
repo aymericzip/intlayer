bb58f1df49606372221f9b03807f13d944bf2c32
Aymeric PINEAU <ay.pineau@gmail.com>
2024-06-26 22:43:50 -0600

    style: style ol markdown

--- packages/@intlayer/design-system/src/components/MarkDownRender/index.tsx
+++ packages/@intlayer/design-system/src/components/MarkDownRender/index.tsx
@@ -14,6 +14,7 @@ type MarkdownRendererProps = {
 
 const StyledInlineCode = tw.strong`shadow-[0_0_10px_-15px_rgba(0,0,0,0.3)] bg-card/60 p-1 rounded dark:bg-card-dark/60 shadow-sm backdrop-blur`;
 const StyledUl = tw.ul`flex flex-col pl-5 mt-5 gap-3`;
+const StyledOl = tw.ul`flex flex-col pl-5 mt-5 gap-3`;
 const StyledWrapper = tw.div`text-text dark:text-text-dark flex flex-col gap-6 p-10`;
 const StyledLink = tw.a`text-neutral dark:text-neutral-dark underline`;
 
@@ -48,6 +49,7 @@ export const MarkdownRenderer: FC<MarkdownRendererProps> = ({
             ),
         },
         ul: StyledUl,
+        ol: StyledOl,
         img: (props) => (
           // eslint-disable-next-line jsx-a11y/alt-text
           <img
