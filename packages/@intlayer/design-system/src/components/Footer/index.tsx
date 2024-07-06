d34cf2af2ff8c13ed08ccd9281b91c2625a28953
Aymeric PINEAU <ay.pineau@gmail.com>
2024-07-06 16:03:24 -0600

    style(design-system): increase gap footer

--- packages/@intlayer/design-system/src/components/Footer/index.tsx
+++ packages/@intlayer/design-system/src/components/Footer/index.tsx
@@ -18,7 +18,7 @@ const StyledFooter = tw.footer`flex flex-auto flex-row justify-around p-6 items-
 const StyledAsideContent = tw.aside`flex flex-col justify-between items-center md:w-1/4`;
 const StyledAsideDescription = tw.span`text-sm text-neutral text-center text-xs`;
 
-const StyledFooterContent = tw.div`w-full flex flex-row justify-around m-auto flex-wrap gap-2 md:w-2/3`;
+const StyledFooterContent = tw.div`w-full flex flex-row justify-around m-auto flex-wrap gap-x-3 gap-y-6 md:w-2/3`;
 const StyledColumn = tw.div`flex flex-col gap-2`;
 const StyledColumnRow = tw.div`flex flex-col gap-3`;
 
