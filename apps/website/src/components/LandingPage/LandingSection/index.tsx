7d835b1544b2ce4817778dd37abab8ebe39bea24
Aymeric PINEAU <ay.pineau@gmail.com>
2024-07-07 22:24:43 -0600

    style(website): reduce padding page mobile

--- apps/website/src/components/LandingPage/LandingSection/index.tsx
+++ apps/website/src/components/LandingPage/LandingSection/index.tsx
@@ -22,7 +22,7 @@ export const LandingSection: FC = () => {
   const { title, description } = useIntlayer('landing-section');
 
   return (
-    <div className="flex w-full flex-col gap-16 p-20 md:flex-row">
+    <div className="flex w-full flex-col gap-16 p-8 sm:p-12 md:flex-row md:p-20">
       <div className="flex w-full flex-col items-center justify-center gap-6 md:h-[75vh] md:w-3/5">
         <AnimatedTitle className="text-4xl font-bold" text={title.value} />
         <AnimatedDescription className=" text-neutral dark:text-neutral-dark">
