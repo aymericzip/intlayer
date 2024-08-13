17e7460d971c89b637ba087e273be9ea20405c13
Aymeric PINEAU <ay.pineau@gmail.com>
2024-08-13 17:45:48 -0600

    feat(website): update SwitchThemeSwitcher accessibility

--- apps/website/src/components/ThemeSwitcherDropDown/SwitchThemeSwitcher.tsx
+++ apps/website/src/components/ThemeSwitcherDropDown/SwitchThemeSwitcher.tsx
@@ -14,11 +14,19 @@ export const SwitchThemeSwitcher: FC = () => {
 
   const themeSwitcher = [
     {
-      content: <SunIcon size={20} />,
+      content: (
+        <SunIcon
+          size={20}
+          data-mode="light"
+          aria-label="Switch to light mode"
+        />
+      ),
       value: Modes.light,
     },
     {
-      content: <MoonIcon size={20} />,
+      content: (
+        <MoonIcon size={20} data-mode="dark" aria-label="Switch to dark mode" />
+      ),
       value: Modes.dark,
     },
   ] as SwitchSelectorChoices<Modes>;
