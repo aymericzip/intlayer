28b8d5f52c72955a31090783b9d246586bd0991d
Aymeric PINEAU <ay.pineau@gmail.com>
2024-08-14 01:13:43 -0600

    feat(website): adapt ProductHunt to mobile

--- apps/website/src/components/ProductHunt/index.tsx
+++ apps/website/src/components/ProductHunt/index.tsx
@@ -1,34 +1,52 @@
 'use client';
 
 import { Container, MaxWidthSmoother } from '@intlayer/design-system';
+import { useDevice } from '@intlayer/design-system/hooks';
+import { cn } from '@utils/cn';
 import { X } from 'lucide-react';
 import Link from 'next/link';
 import { useIntlayer } from 'next-intlayer';
 import { useEffect, useState } from 'react';
 import ProductHuntLogo from './product-hunt-logo.svg';
 
-const MINIATURIZING_TIME = 8000;
+const VISIBLE_START_TIME = 3000;
+const MINIATURIZING_END_TIME = 6000;
 
 export const ProductHunt = () => {
-  const [isVisible, setIsVisible] = useState(true);
+  const { isMobile } = useDevice();
+  const [isVisible, setIsVisible] = useState(false);
   const [isMiniaturized, setIsMiniaturized] = useState(false);
   const { title, content, details } = useIntlayer('product-hunt');
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsMiniaturized(true);
-    }, MINIATURIZING_TIME);
+    }, MINIATURIZING_END_TIME);
 
     return () => clearTimeout(timer);
-  }, [isVisible]);
+  }, [isMobile, isVisible]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, VISIBLE_START_TIME);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   if (!isVisible) {
     return null;
   }
 
+  const isMiniaturizable = !isMobile && isMiniaturized;
+
   return (
     <Container
-      className="border-text group fixed bottom-5 left-5 z-[100] border-2 p-2"
+      className={cn([
+        'border-text dark:border-text-dark group fixed bottom-5 left-5 z-[100] mr-5 border-2 p-2 transition',
+        'hover:translate-x-0 hover:translate-y-0 hover:scale-100',
+        isMiniaturizable && '-translate-x-1/4 translate-y-1/4 scale-50',
+      ])}
       roundedSize="2xl"
     >
       <Link
@@ -36,7 +54,7 @@ export const ProductHunt = () => {
         target="_blank"
         rel="noreferrer"
       >
-        <div className="flex flex-row gap-6 rounded-lg p-3 ">
+        <div className="flex flex-row gap-6 rounded-lg p-3">
           <div className="flex flex-col items-center gap-2 rounded-lg">
             <ProductHuntLogo className="size-[80px]" />
             <div className="flex flex-col items-center rounded-lg transition group-hover:scale-110">
@@ -45,8 +63,8 @@ export const ProductHunt = () => {
             </div>
           </div>
           <MaxWidthSmoother
-            isHidden={isMiniaturized}
-            className="group-hover:grid-cols-[1fr]"
+            isHidden={isMiniaturizable}
+            className="max-md:grid-cols-[1fr]! group-hover:grid-cols-[1fr]"
           >
             <div className="flex w-72 flex-col gap-2 overflow-hidden rounded-lg">
               <strong className="text-xl uppercase">{title}</strong>
