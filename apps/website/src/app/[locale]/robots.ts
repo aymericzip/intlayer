5bce6a0f7c1cd22f7b376c47a447ac1c6d4233ed
Aymeric PINEAU <ay.pineau@gmail.com>
2024-08-14 16:27:24 -0600

    feat(website): update disallow robot.txt 404

--- apps/website/src/app/[locale]/robots.ts
+++ apps/website/src/app/[locale]/robots.ts
@@ -1,10 +1,11 @@
 import type { MetadataRoute } from 'next';
+import { locales } from '../../../intlayer.config';
 
 const robots = (): MetadataRoute.Robots => ({
   rules: {
     userAgent: '*',
     allow: '/',
-    disallow: ['/404'],
+    disallow: ['/404', ...locales.map((locale) => `/${locale}/404`)],
   },
   sitemap: 'https://acme.com/sitemap.xml',
 });
