debfc136a73cd2c1d79bd60be5afb739951d1480
Aymeric PINEAU <ay.pineau@gmail.com>
2024-08-19 11:50:52 -0600

    feat(website): remove product hunt

--- apps/website/src/app/[locale]/(not-editable)/page.tsx
+++ apps/website/src/app/[locale]/(not-editable)/page.tsx
@@ -1,12 +1,10 @@
 import { LandingPage } from '@components/LandingPage';
-import { ProductHunt } from '@components/ProductHunt';
 import type { NextPageIntlayer } from 'next-intlayer';
 import { IntlayerServerProvider } from 'next-intlayer/server';
 
 const Page: NextPageIntlayer = ({ params: { locale } }) => (
   <IntlayerServerProvider locale={locale}>
     <LandingPage />
-    <ProductHunt />
   </IntlayerServerProvider>
 );
 export default Page;
