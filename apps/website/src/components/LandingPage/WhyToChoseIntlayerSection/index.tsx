9da1ed2456815104db5536a7b53b62bb55c02b66
Aymeric PINEAU <ay.pineau@gmail.com>
2024-08-26 14:11:10 -0600

    feat(website): update whyToChoseIntlayerContent icons

--- apps/website/src/components/LandingPage/WhyToChoseIntlayerSection/index.tsx
+++ apps/website/src/components/LandingPage/WhyToChoseIntlayerSection/index.tsx
@@ -1,6 +1,16 @@
+import { Code, Timer, ListTree, DollarSign, Type, Pencil } from 'lucide-react';
 import { useIntlayer } from 'next-intlayer/server';
 import type { FC } from 'react';
 import { AnimatedDiv } from './AnimatedDiv';
+
+const iconKeyMap = {
+  code: Code,
+  type: Type,
+  free: DollarSign,
+  'file-tree': ListTree,
+  timer: Timer,
+  cms: Pencil,
+};
 
 export const WhyToChoseIntlayerSection: FC = () => {
   const { title, content } = useIntlayer('why-to-chose-intlayer-section');
@@ -10,24 +20,29 @@ export const WhyToChoseIntlayerSection: FC = () => {
       <h2 className="text-neutral dark:text-neutral-dark">{title}</h2>
 
       <div className="m-auto mt-3 grid w-full grid-cols-[auto_auto] items-center justify-evenly justify-items-center gap-x-2 gap-y-10 px-3 py-2 sm:grid-cols-[1fr_1fr_1fr]">
-        {content.map((asset, index) => (
-          <AnimatedDiv
-            className="flex max-w-[180px] flex-col items-center gap-3 text-center md:max-w-[200px]"
-            key={index}
-          >
-            <span className="flex size-12 items-center justify-center rounded-full border-4 border-lime-300 text-2xl text-lime-800 dark:border-lime-900 dark:text-lime-600">
-              {index + 1}
-            </span>
-            <strong className="md:text-md text-sm font-semibold md:text-lg">
-              {asset.title}
-            </strong>
-            <AnimatedDiv>
-              <p className="text-neutral dark:text-neutral-dark text-xs">
-                {asset.descrition}
-              </p>
+        {content.map((asset, index) => {
+          const Icon =
+            iconKeyMap[asset.iconKey.value as keyof typeof iconKeyMap];
+
+          return (
+            <AnimatedDiv
+              className="flex max-w-[180px] flex-col items-center gap-3 text-center md:max-w-[200px]"
+              key={index}
+            >
+              <span className="flex size-12 items-center justify-center rounded-full border-4 border-lime-300 text-2xl text-lime-800 dark:border-lime-900 dark:text-lime-600">
+                <Icon className="size-5" />
+              </span>
+              <strong className="md:text-md text-sm font-semibold md:text-lg">
+                {asset.title}
+              </strong>
+              <AnimatedDiv>
+                <p className="text-neutral dark:text-neutral-dark text-xs">
+                  {asset.description}
+                </p>
+              </AnimatedDiv>
             </AnimatedDiv>
-          </AnimatedDiv>
-        ))}
+          );
+        })}
       </div>
     </section>
   );
--- apps/website/src/components/LandingPage/WhyToChoseIntlayerSection/why-to-chose-intlayer.content.ts
+++ apps/website/src/components/LandingPage/WhyToChoseIntlayerSection/why-to-chose-intlayer.content.ts
@@ -1,86 +1,102 @@
 import { t, type DeclarationContent } from 'intlayer';
 
-const whyToChoseIntlayerContent: DeclarationContent = {
-  id: 'why-to-chose-intlayer-section',
-  title: t({
-    en: 'Why to chose Inlyayer?',
-    fr: 'Pourquoi choisir Intlayer ?',
-    es: '¿Por qué elegir Intlayer?',
-  }),
-  content: [
-    {
-      title: t({
-        en: 'Free and Open Source',
-        es: 'Gratis y de Código Abierto',
-        fr: 'Gratuit et Open Source',
-      }),
-      descrition: t({
-        en: 'Intlayer is free and open source, allowing you to use it for free and contribute to suit your needs.',
-        es: 'Intlayer es gratuito y de código abierto, lo que le permite usarlo gratuitamente y contribuir a adaptarse a sus necesidades.',
-        fr: "Intlayer est gratuit et open source, vous permettant de l'utiliser gratuitement et de contribuer à son bon fonctionnement.",
-      }),
-    },
-    {
-      title: t({
-        en: 'JavaScript-Powered Content Management',
-        es: 'Gestión de Contenido Impulsada por JavaScript',
-        fr: 'Gestion de Contenu Propulsée par JavaScript',
-      }),
-      descrition: t({
-        en: 'Harness the flexibility of JavaScript to define and manage your content efficiently.',
-        es: 'Aprovecha la flexibilidad de JavaScript para definir y gestionar tu contenido eficientemente.',
-        fr: 'Exploitez la flexibilité de JavaScript pour définir et gérer votre contenu efficacement.',
-      }),
-    },
-    {
-      title: t({
-        en: 'Type-Safe Environment',
-        es: 'Type-Safe Entorno',
-        fr: 'Type-Safe Environnement',
-      }),
-      descrition: t({
-        en: 'Leverage TypeScript to ensure all your content definitions are precise and error-free.',
-        es: 'Utiliza TypeScript para asegurar que todas tus definiciones de contenido sean precisas y sin errores.',
-        fr: "Utilisez TypeScript pour garantir que toutes vos définitions de contenu soient précises et exemptes d'erreurs.",
-      }),
-    },
-    {
-      title: t({
-        en: 'Declaration at the component level',
-        es: 'Declaración a nivel de componente',
-        fr: 'Declaration au niveau du component',
-      }),
-      descrition: t({
-        en: 'Keep your translations close to their respective components, enhancing maintainability and clarity.',
-        es: 'Mantén tus traducciones cerca de sus respectivos componentes, mejorando la mantenibilidad y claridad.',
-        fr: 'Gardez vos traductions proches de leurs composants respectifs, améliorant la maintenabilité et la clarté.',
-      }),
-    },
-    {
-      title: t({
-        en: 'Simplified Setup',
-        es: 'Configuración Simplificada',
-        fr: 'Configuration Simplifiée',
-      }),
-      descrition: t({
-        en: 'Get up and running quickly with minimal configuration, especially optimized for Next.js projects.',
-        es: 'Ponte en marcha rápidamente con una configuración mínima, especialmente optimizado para proyectos Next.js.',
-        fr: 'Mettez-vous en route rapidement avec une configuration minimale, spécialement optimisée pour les projets Next.js.',
-      }),
-    },
-    {
-      title: t({
-        en: 'Integrated CMS',
-        es: 'CMS Integrado',
-        fr: 'CMS Intégré',
-      }),
-      descrition: t({
-        en: 'Edit your content on your website with an integrated CMS directly on your website. Boost content generation thanks to AI.',
-        es: 'Edite sus contenidos con un CMS directamente integrado en su sitio web. Aumente la generación de contenido gracias a la IA.',
-        fr: "Editez vos contenus à l'aide un CMS directement intégré à votre site. Boostez la generation de contenu grâce à l'IA.",
-      }),
-    },
-  ],
+type WhyToChoseIntlayerContent = {
+  title: string;
+  content: {
+    title: string;
+    description: string;
+    iconKey: string;
+  }[];
 };
+
+const whyToChoseIntlayerContent: DeclarationContent<WhyToChoseIntlayerContent> =
+  {
+    id: 'why-to-chose-intlayer-section',
+    title: t({
+      en: 'Why to chose Inlyayer?',
+      fr: 'Pourquoi choisir Intlayer ?',
+      es: '¿Por qué elegir Intlayer?',
+    }),
+    content: [
+      {
+        title: t({
+          en: 'Free and Open Source',
+          es: 'Gratis y de Código Abierto',
+          fr: 'Gratuit et Open Source',
+        }),
+        description: t({
+          en: 'Intlayer is free and open source, allowing you to use it for free and contribute to suit your needs.',
+          es: 'Intlayer es gratuito y de código abierto, lo que le permite usarlo gratuitamente y contribuir a adaptarse a sus necesidades.',
+          fr: "Intlayer est gratuit et open source, vous permettant de l'utiliser gratuitement et de contribuer à son bon fonctionnement.",
+        }),
+        iconKey: 'free',
+      },
+      {
+        title: t({
+          en: 'JavaScript-Powered Content Management',
+          es: 'Gestión de Contenido Impulsada por JavaScript',
+          fr: 'Gestion de Contenu Propulsée par JavaScript',
+        }),
+        description: t({
+          en: 'Harness the flexibility of JavaScript to define and manage your content efficiently.',
+          es: 'Aprovecha la flexibilidad de JavaScript para definir y gestionar tu contenido eficientemente.',
+          fr: 'Exploitez la flexibilité de JavaScript pour définir et gérer votre contenu efficacement.',
+        }),
+        iconKey: 'code',
+      },
+      {
+        title: t({
+          en: 'Type-Safe Environment',
+          es: 'Type-Safe Entorno',
+          fr: 'Type-Safe Environnement',
+        }),
+        description: t({
+          en: 'Leverage TypeScript to ensure all your content definitions are precise and error-free.',
+          es: 'Utiliza TypeScript para asegurar que todas tus definiciones de contenido sean precisas y sin errores.',
+          fr: "Utilisez TypeScript pour garantir que toutes vos définitions de contenu soient précises et exemptes d'erreurs.",
+        }),
+        iconKey: 'type',
+      },
+      {
+        title: t({
+          en: 'Declaration at the component level',
+          es: 'Declaración a nivel de componente',
+          fr: 'Declaration au niveau du component',
+        }),
+        description: t({
+          en: 'Keep your translations close to their respective components, enhancing maintainability and clarity.',
+          es: 'Mantén tus traducciones cerca de sus respectivos componentes, mejorando la mantenibilidad y claridad.',
+          fr: 'Gardez vos traductions proches de leurs composants respectifs, améliorant la maintenabilité et la clarté.',
+        }),
+        iconKey: 'file-tree',
+      },
+      {
+        title: t({
+          en: 'Simplified Setup',
+          es: 'Configuración Simplificada',
+          fr: 'Configuration Simplifiée',
+        }),
+        description: t({
+          en: 'Get up and running quickly with minimal configuration, especially optimized for Next.js projects.',
+          es: 'Ponte en marcha rápidamente con una configuración mínima, especialmente optimizado para proyectos Next.js.',
+          fr: 'Mettez-vous en route rapidement avec une configuration minimale, spécialement optimisée pour les projets Next.js.',
+        }),
+        iconKey: 'timer',
+      },
+      {
+        title: t({
+          en: 'Integrated CMS',
+          es: 'CMS Integrado',
+          fr: 'CMS Intégré',
+        }),
+        description: t({
+          en: 'Edit your content on your website with an integrated CMS directly on your website. Boost content generation thanks to AI.',
+          es: 'Edite sus contenidos con un CMS directamente integrado en su sitio web. Aumente la generación de contenido gracias a la IA.',
+          fr: "Editez vos contenus à l'aide un CMS directement intégré à votre site. Boostez la generation de contenu grâce à l'IA.",
+        }),
+        iconKey: 'cms',
+      },
+    ],
+  };
 
 export default whyToChoseIntlayerContent;
