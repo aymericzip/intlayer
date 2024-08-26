a9a12e97cc915b8c290274e05226cea81244d40e
Aymeric PINEAU <ay.pineau@gmail.com>
2024-08-25 23:21:49 -0600

    fix(design-system): fix InputPassword click eye

--- packages/@intlayer/design-system/src/components/Input/InputPassword.tsx
+++ packages/@intlayer/design-system/src/components/Input/InputPassword.tsx
@@ -1,17 +1,40 @@
 'use client';
 
-import { EyeIcon, EyeOffIcon } from 'lucide-react';
-import { useState, forwardRef } from 'react';
+import {
+  EyeIcon,
+  EyeOffIcon,
+  type LucideProps,
+  type LucideIcon,
+} from 'lucide-react';
+import { useState, forwardRef, type MouseEventHandler, type FC } from 'react';
+import tw from 'twin.macro';
 import { Input, type InputProps } from '.';
 
 type InputPasswordProps = Omit<InputProps, 'type'>;
+
+const StyledButton = tw.button`absolute right-2 h-full flex-row items-center`;
+
+type EyeIconComponentProps = {
+  Icon: LucideIcon;
+} & LucideProps;
+
+const EyeIconComponent: FC<EyeIconComponentProps> = ({ Icon, ...props }) => (
+  <Icon {...props} />
+);
+
+const StyledEyeIconComponent = tw(
+  EyeIconComponent
+)`text-neutral dark:text-neutral-dark mr-2 inline-block`;
 
 export const InputPassword = forwardRef<HTMLInputElement, InputPasswordProps>(
   (props, ref) => {
     const [isPasswordRevealed, setIsPasswordRevealed] = useState(false);
 
-    const handlePasswordReveal = () =>
+    const handlePasswordReveal: MouseEventHandler<HTMLButtonElement> = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
       setIsPasswordRevealed((isPasswordRevealed) => !isPasswordRevealed);
+    };
 
     const EyeIconComponent = isPasswordRevealed ? EyeIcon : EyeOffIcon;
 
@@ -23,17 +46,14 @@ export const InputPassword = forwardRef<HTMLInputElement, InputPasswordProps>(
           type={isPasswordRevealed ? 'text' : 'password'}
         />
 
-        <button
+        <StyledButton
           data-testid="eye-icon"
           className="absolute right-2 h-full flex-row items-center"
           onClick={handlePasswordReveal}
           aria-label={isPasswordRevealed ? 'Hide password' : 'Show password'}
         >
-          <EyeIconComponent
-            className="color-slate-700 mr-2 inline-block"
-            size={20}
-          />
-        </button>
+          <StyledEyeIconComponent Icon={EyeIconComponent} size={20} />
+        </StyledButton>
       </div>
     );
   }
