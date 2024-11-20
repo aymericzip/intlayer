495be76d740adace20fb12ea83e9c5d6239dc020
Aymeric PINEAU <ay.pineau@gmail.com>
2024-11-19 23:06:27 -0600

    fix(website): update SetupOrganizationStepForm

--- apps/website/src/components/OnboardPage/SetUpOrganizationStep/SetUpOrganizationStepForm.tsx
+++ apps/website/src/components/OnboardPage/SetUpOrganizationStep/SetUpOrganizationStepForm.tsx
@@ -11,6 +11,7 @@ import {
   Modal,
   H2,
   useToast,
+  useAuth,
 } from '@intlayer/design-system';
 import {
   useGetOrganizations,
@@ -18,10 +19,10 @@ import {
 } from '@intlayer/design-system/hooks';
 import { useIntlayer } from 'next-intlayer';
 import {
-  forwardRef,
   HTMLAttributes,
   Suspense,
   useEffect,
+  useMemo,
   useState,
   type FC,
 } from 'react';
@@ -72,18 +73,34 @@ const OrganizationFormContent: FC<{
 
 export const SetupOrganizationStepForm: FC = () => {
   const SetUpOrganizationSchema = getSetUpOrganizationSchema();
+  const { session } = useAuth();
   const { selectOrganization } = useSelectOrganization();
   const { toast } = useToast();
   const { formData, goNextStep, goPreviousStep, setFormData } = useStep(
     Steps.SetupOrganization
   );
+
+  const defaultValues = useMemo(
+    () => ({
+      ...formData,
+      organizationId: session?.organization?._id
+        ? String(session?.organization?._id)
+        : formData?.organizationId,
+    }),
+    [formData, session]
+  );
+
   const { form, isSubmitting } = useForm(SetUpOrganizationSchema, {
-    defaultValues: formData,
+    defaultValues,
   });
   const { title, successToast } = useIntlayer('set-up-organization-step');
 
   const onSubmitSuccess = async (data: SetUpOrganization) => {
     setFormData(data);
+
+    if (String(data.organizationId) === String(session?.organization?._id)) {
+      goNextStep();
+    }
 
     if (formData?.organizationId) {
       await selectOrganization(formData.organizationId).then(() => {
@@ -99,8 +116,8 @@ export const SetupOrganizationStepForm: FC = () => {
 
   useEffect(() => {
     // Reset the form to the initial state once loaded from the session storage
-    form.reset(formData);
-  }, [formData, form]);
+    form.reset(defaultValues);
+  }, [defaultValues, form]);
 
   return (
     <Form
@@ -108,7 +125,12 @@ export const SetupOrganizationStepForm: FC = () => {
       onSubmitSuccess={onSubmitSuccess}
       {...form}
     >
-      <StepLayout onGoToPreviousStep={goPreviousStep} isLoading={isSubmitting}>
+      <StepLayout
+        onGoToPreviousStep={goPreviousStep}
+        isLoading={isSubmitting}
+        isSkippable={Boolean(formData?.organizationId)}
+        onSkipStep={goNextStep}
+      >
         <H2>{title}</H2>
         <Suspense fallback={<Loader />}>
           <Form.Element
