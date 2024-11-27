d2ea99ec7e9da80536d1a48b2d0e8f8787e47d32
Aymeric PINEAU <ay.pineau@gmail.com>
2024-11-27 15:31:34 -0500

    fix(website): fix build

--- apps/website/src/app/[locale]/(landing)/(not-editable)/page.tsx
+++ apps/website/src/app/[locale]/(landing)/(not-editable)/page.tsx
@@ -1,7 +1,7 @@
 import { BackgroundLayout } from '@components/BackgroundLayout';
 import { LandingPage as LandingPageContent } from '@components/LandingPage';
-import { SoftwareApplicationHeader } from '@structuredData/FAQPageHeader';
 import { ProductHeader } from '@structuredData/ProductHeader';
+import { SoftwareApplicationHeader } from '@structuredData/SoftwareApplication';
 import { WebsiteHeader } from '@structuredData/WebsiteHeader';
 import type { NextPageIntlayer } from 'next-intlayer';
 import { IntlayerServerProvider } from 'next-intlayer/server';
