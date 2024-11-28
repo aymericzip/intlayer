f76b2b61a6fceb108953d3cfac579ecae76fb250
Aymeric PINEAU <ay.pineau@gmail.com>
2024-11-28 16:53:38 -0500

    fix(website): wrap DocumentationSearchPage with Suspense

--- apps/website/src/app/[locale]/(landing)/(not-editable)/doc/search/page.tsx
+++ apps/website/src/app/[locale]/(landing)/(not-editable)/doc/search/page.tsx
@@ -1,20 +1,23 @@
 import { SearchView } from '@components/Search/SearchView';
-import { Container } from '@intlayer/design-system';
+import { Container, Loader } from '@intlayer/design-system';
 import { WebsiteHeader } from '@structuredData/WebsiteHeader';
 import type { NextPageIntlayer } from 'next-intlayer';
 import { IntlayerServerProvider } from 'next-intlayer/server';
+import { Suspense } from 'react';
 
-const DocumentationPage: NextPageIntlayer = ({ params: { locale } }) => (
+const DocumentationSearchPage: NextPageIntlayer = ({ params: { locale } }) => (
   <>
     <WebsiteHeader />
     <IntlayerServerProvider locale={locale}>
       <div className="flex size-full flex-1 items-baseline p-10 md:mt-[10vh]">
         <Container className="mx-auto w-full max-w-3xl p-10" roundedSize="2xl">
-          <SearchView />
+          <Suspense fallback={<Loader />}>
+            <SearchView />
+          </Suspense>
         </Container>
       </div>
     </IntlayerServerProvider>
   </>
 );
 
-export default DocumentationPage;
+export default DocumentationSearchPage;
