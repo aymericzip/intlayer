9e7fb8d834fdf44a9683d41eda306a78cfb98b7d
Aymeric PINEAU <ay.pineau@gmail.com>
2024-11-28 00:58:33 -0500

    fix(website): fix content landing page

--- apps/website/src/components/LandingPage/LandingSection/landing-section.content.ts
+++ apps/website/src/components/LandingPage/LandingSection/landing-section.content.ts
@@ -9,9 +9,9 @@ const landingSectionContent = {
       es: 'Traduzca su sitio web de manera más eficiente',
     }),
     description: t({
-      en: 'Intlayer is an internationalization library designed specifically for JavaScript developers. It allow the declaration of your content everywhere in your code. It converts declaration of multilingual content into structured dictionaries to integrate easily in your code. Using TypeScript, Intlayer make your development stronger and more efficient.',
+      en: 'Intlayer is an internationalization library designed specifically for JavaScript developers. It allows the declaration of your content throughout your code. It converts multilingual content declarations into structured dictionaries, making integration easy. Using TypeScript, Intlayer strengthens your development and enhances efficiency.',
       fr: "Intlayer est une librarie d'internationalisation conçue spécifiquement pour les développeurs JavaScript. Elle permet la déclaration de votre contenu à n'importe quel endroit dans votre code. Elle convertit la déclaration de votre contenu multilingue en dictionnaires structurés pour faciliter l'intégration dans votre codebase. En utilisant TypeScript, Intlayer rend votre developpement plus fort et efficace.",
-      es: 'Intlayer es una librería de internationalization diseñada específicamente para desarrolladores de JavaScript. Permite la declaración de su contenido en cualquier lugar en su código. Convierte la declaración de su contenido multilingüe en diccionarios estructurados para facilitar la integración en su base de código. Utilizando TypeScript, Intlayer hace que el desarrollo sea más fuerte y eficiente.',
+      es: 'Intlayer es una librería de internacionalización diseñada específicamente para desarrolladores de JavaScript. Permite la declaración de su contenido en cualquier parte de su código. Convierte la declaración de contenido multilingüe en diccionarios estructurados para facilitar la integración en su base de código. Al utilizar TypeScript, Intlayer fortalece su desarrollo y lo hace más eficiente.',
     }),
   },
 } satisfies DeclarationContent;
