07047d8bb7c60645c8816fd2fe6e456fb2753b43
Aymeric PINEAU <ay.pineau@gmail.com>
2025-01-19 01:40:56 +0100

    feat: export generateMetadata from layout component

--- apps/website/src/app/[locale]/(landing)/layout.tsx
+++ apps/website/src/app/[locale]/(landing)/layout.tsx
@@ -1,6 +1,8 @@
 import { PageLayout } from '@layouts/PageLayout';
 import type { Next14LayoutIntlayer } from 'next-intlayer';
 
+export { generateMetadata } from './metadata';
+
 const LocaleLayout: Next14LayoutIntlayer = ({
   children,
   params: { locale },
