7377584da7d55e50ec6e7f4617f9a809bcae073f
Aymeric PINEAU <ay.pineau@gmail.com>
2025-01-19 15:56:31 +0100

    feat: add start script and enable main app components in App.tsx

--- packages/intlayer-editor/eslint.config.js
+++ packages/intlayer-editor/eslint.config.js
@@ -1,23 +1,6 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import typeScriptParser from '@typescript-eslint/parser';
 import baseConfig from '@utils/eslint-config';
 
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
-
 /** @type {import('eslint').Linter.Config[]} */
-const config = [
-  baseConfig,
-  {
-    languageOptions: {
-      parser: typeScriptParser,
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: 'tsconfig.json',
-      },
-    },
-  },
-];
+const config = [...baseConfig];
 
 export default config;
--- packages/intlayer-editor/src/App.tsx
+++ packages/intlayer-editor/src/App.tsx
@@ -1,21 +1,20 @@
-// import { IframeController } from './components/IframeController';
-// import { AppRouter } from './components/AppRouter';
-// import { AppLayout } from './components/AppLayout';
-// import { AppProvider } from './components/AppProvider';
-// import { useRef } from 'react';
+import { useRef } from 'react';
+import { AppLayout } from './components/AppLayout';
+import { AppProvider } from './components/AppProvider';
+import { AppRouter } from './components/AppRouter';
+import { IframeController } from './components/IframeController';
 
 const App = () => {
-  // const iframeRef = useRef<HTMLIFrameElement>(null);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
 
   return (
-    <></>
-    // <AppProvider iframeRef={iframeRef}>
-    //   <AppRouter>
-    //     <AppLayout>
-    //       <IframeController iframeRef={iframeRef} />
-    //     </AppLayout>
-    //   </AppRouter>
-    // </AppProvider>
+    <AppProvider iframeRef={iframeRef}>
+      <AppRouter>
+        <AppLayout>
+          <IframeController iframeRef={iframeRef} />
+        </AppLayout>
+      </AppRouter>
+    </AppProvider>
   );
 };
 
