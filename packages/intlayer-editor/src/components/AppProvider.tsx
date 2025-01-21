00a14e0d9aeac2675ca576c94aa75ce21bbbda3e
Aymeric PINEAU <ay.pineau@gmail.com>
2025-01-21 21:23:33 +0100

    refactor(intlayer-editor): remove editor alert

--- packages/intlayer-editor/src/components/AppProvider.tsx
+++ packages/intlayer-editor/src/components/AppProvider.tsx
@@ -1,14 +1,8 @@
 'use client';
 
-import { appLogger, getConfiguration } from '@intlayer/config/client';
 import { AsyncStateProvider } from '@intlayer/design-system/hooks';
 import { EditorProvider } from '@intlayer/editor-react';
-import {
-  type FC,
-  type PropsWithChildren,
-  type RefObject,
-  useEffect,
-} from 'react';
+import { type FC, type PropsWithChildren, type RefObject } from 'react';
 
 /**
  * Provider that store the current locale on the client side
@@ -17,39 +11,13 @@ export const AppProvider: FC<
   PropsWithChildren<{
     iframeRef: RefObject<HTMLIFrameElement>;
   }>
-> = ({ children, iframeRef }) => {
-  const { editor } = getConfiguration();
-
-  useEffect(() => {
-    if (editor.enabled) {
-      if (!editor.clientId) {
-        appLogger(
-          'Editor is enabled but clientId is not set. Please set it in the editor configuration. See http://localhost:3000/doc/concept/editor.',
-          {
-            level: 'error',
-          }
-        );
-      }
-
-      if (!editor.clientSecret) {
-        appLogger(
-          'Editor is enabled but clientSecret is not set. Please set it in the editor configuration. See http://localhost:3000/doc/concept/editor.',
-          {
-            level: 'error',
-          }
-        );
-      }
+> = ({ children, iframeRef }) => (
+  <EditorProvider
+    postMessage={(data) =>
+      iframeRef.current?.contentWindow?.postMessage(data, '*')
     }
-  }, [editor.enabled, editor.clientId, editor.clientSecret]);
-
-  return (
-    <EditorProvider
-      postMessage={(data) =>
-        iframeRef.current?.contentWindow?.postMessage(data, '*')
-      }
-      allowedOrigins={['*']}
-    >
-      <AsyncStateProvider>{children}</AsyncStateProvider>
-    </EditorProvider>
-  );
-};
+    allowedOrigins={['*']}
+  >
+    <AsyncStateProvider>{children}</AsyncStateProvider>
+  </EditorProvider>
+);
