5f3c757742d888b60a4bb1e2e1f281790748c391
Aymeric PINEAU <ay.pineau@gmail.com>
2025-01-21 21:08:40 +0100

    fix(intlayer-editor): add gitignore

--- packages/intlayer-editor/types/intlayer.d.ts
+++ packages/intlayer-editor/types/intlayer.d.ts
@@ -5,7 +5,7 @@ import type { DictionaryEditionDrawerContent as _qBBua9xjcMgNI6rAYznh } from '..
 declare module 'intlayer' {
   interface IntlayerDictionaryTypesConnector {
     "dictionary-edition-drawer": _qBBua9xjcMgNI6rAYznh;
-  }
+  }mmm
 
   type ConfigLocales = Locales.ENGLISH | Locales.ENGLISH_UNITED_KINGDOM | Locales.FRENCH | Locales.SPANISH | Locales.GERMAN | Locales.JAPANESE | Locales.KOREAN | Locales.CHINESE | Locales.ITALIAN | Locales.PORTUGUESE | Locales.HINDI | Locales.ARABIC | Locales.RUSSIAN;
   type ExtractedLocales = Extract<Locales, ConfigLocales>;
