59f042d208a13e58075d55af0cf6f1fd0428f870
Aymeric PINEAU <ay.pineau@gmail.com>
2025-01-28 00:07:30 +0100

    fix: reorder environment file loading paths for better precedence

--- packages/@intlayer/config/src/envVariables/loadEnvFile.ts
+++ packages/@intlayer/config/src/envVariables/loadEnvFile.ts
@@ -4,4 +4,4 @@ import dotenv from 'dotenv';
 const env = process.env.NODE_ENV ?? 'development';
 
 export const loadEnvFile = () =>
-  dotenv.config({ path: ['.env', `.env.${env}`, `.env.${env}.local`] });
+  dotenv.config({ path: [`.env.${env}.local`, `.env.${env}`, '.env'] });
