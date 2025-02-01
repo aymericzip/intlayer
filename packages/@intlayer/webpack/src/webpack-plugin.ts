5b59cb11421d4a79ad37a34cc16fa43dfee174e7
Aymeric PINEAU <ay.pineau@gmail.com>
2025-02-01 19:47:03 +0100

    fix(webpack): fix IntlayerPlugin to build dictionaries before the app build

--- packages/@intlayer/webpack/src/webpack-plugin.ts
+++ packages/@intlayer/webpack/src/webpack-plugin.ts
@@ -1,12 +1,15 @@
 import { buildAndWatchIntlayer } from '@intlayer/chokidar';
+import { Compiler } from 'webpack';
 
 // Watch mode or on time build
 export class IntlayerPlugin {
-  async apply(): Promise<void> {
-    try {
-      await buildAndWatchIntlayer();
-    } catch (error) {
-      console.error('Error in IntlayerPlugin:', error);
-    }
+  async apply(compiler: Compiler): Promise<void> {
+    compiler.hooks.beforeCompile.tapPromise('IntlayerPlugin', async () => {
+      try {
+        await buildAndWatchIntlayer();
+      } catch (error) {
+        console.error('Error in IntlayerPlugin:', error);
+      }
+    });
   }
 }
