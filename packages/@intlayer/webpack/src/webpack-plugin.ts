e02240d20cc14329df646cfac30458587f6a13ff
Aymeric PINEAU <ay.pineau@gmail.com>
2025-02-06 00:26:28 +0100

    fix(webpack-plugin): prevent infinite reloading on CRA and next by adding isWatching flag

--- packages/@intlayer/webpack/src/webpack-plugin.ts
+++ packages/@intlayer/webpack/src/webpack-plugin.ts
@@ -3,12 +3,17 @@ import { Compiler } from 'webpack';
 
 // Watch mode or on time build
 export class IntlayerPlugin {
+  private isWatching = false; // Flag to ensure we only start the watcher once
+
   async apply(compiler: Compiler): Promise<void> {
     compiler.hooks.beforeCompile.tapPromise('IntlayerPlugin', async () => {
-      try {
-        await buildAndWatchIntlayer();
-      } catch (error) {
-        console.error('Error in IntlayerPlugin:', error);
+      if (!this.isWatching) {
+        try {
+          await buildAndWatchIntlayer();
+          this.isWatching = true;
+        } catch (error) {
+          console.error('Error in IntlayerPlugin:', error);
+        }
       }
     });
   }
