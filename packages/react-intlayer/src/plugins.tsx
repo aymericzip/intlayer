d6f9868ef0e4bb5164e04eb951d527487cdf6ea1
Aymeric PINEAU <ay.pineau@gmail.com>
2025-02-07 19:58:53 +0100

    refactor(react-intlayer): relocate renderReactElement for improved element rendering

--- packages/react-intlayer/src/plugins.tsx
+++ packages/react-intlayer/src/plugins.tsx
@@ -6,10 +6,11 @@ import {
   type MarkdownContent,
   NodeType,
 } from '@intlayer/core';
-import { type ReactElement, type ReactNode, createElement } from 'react';
+import type { ReactNode } from 'react';
 import { renderIntlayerEditor } from './editor';
 import type { IntlayerNode } from './IntlayerNode';
 import { renderMarkdown } from './markdown/renderMarkdown';
+import { renderReactElement } from './reactElement/renderReactElement';
 
 /** ---------------------------------------------
  *  INTLAYER NODE PLUGIN
@@ -46,53 +47,6 @@ export type ReactNodeCond<T, _S> = T extends {
   ? ReactNode
   : never;
 
-// This function recursively creates React elements from a given JSON-like structure
-const createReactElement = (element: ReactElement) => {
-  if (typeof element === 'string') {
-    // If it's a string, simply return it (used for text content)
-    return element;
-  }
-
-  const convertChildrenAsArray = (
-    element: ReactElement<{ children?: ReactNode }>
-  ): ReactElement<{ children?: ReactNode }> => {
-    if (element?.props && typeof element.props.children === 'object') {
-      const childrenResult: ReactNode[] = [];
-      const { children } = element.props;
-
-      // Create the children elements recursively, if any
-      Object.keys(children ?? {}).forEach((key) => {
-        childrenResult.push(
-          createReactElement((children ?? {})[key as keyof typeof children])
-        );
-      });
-
-      return {
-        ...element,
-        props: { ...element.props, children: childrenResult },
-      };
-    }
-
-    return {
-      ...element,
-      props: { ...element.props, children: element.props.children },
-    };
-  };
-
-  const fixedElement = convertChildrenAsArray(
-    element as ReactElement<{ children?: ReactNode }>
-  );
-
-  const { type, props } = fixedElement;
-
-  // Create and return the React element
-  return createElement(
-    type ?? 'div',
-    props,
-    ...(props.children as ReactNode[])
-  );
-};
-
 /** Translation plugin. Replaces node with a locale string if nodeType = Translation. */
 export const reactNodePlugins: Plugins = {
   canHandle: (node) =>
@@ -101,7 +55,7 @@ export const reactNodePlugins: Plugins = {
     typeof node.props !== 'undefined' &&
     typeof node.key !== 'undefined',
 
-  transform: createReactElement,
+  transform: renderReactElement,
 };
 
 /**
---
+++ packages/react-intlayer/src/reactElement/renderReactElement.ts
@@ -0,0 +1,48 @@
+import { type ReactElement, type ReactNode, createElement } from 'react';
+
+// This function recursively creates React elements from a given JSON-like structure
+export const renderReactElement = (element: ReactElement) => {
+  if (typeof element === 'string') {
+    // If it's a string, simply return it (used for text content)
+    return element;
+  }
+
+  const convertChildrenAsArray = (
+    element: ReactElement<{ children?: ReactNode }>
+  ): ReactElement<{ children?: ReactNode }> => {
+    if (element?.props && typeof element.props.children === 'object') {
+      const childrenResult: ReactNode[] = [];
+      const { children } = element.props;
+
+      // Create the children elements recursively, if any
+      Object.keys(children ?? {}).forEach((key) => {
+        childrenResult.push(
+          renderReactElement((children ?? {})[key as keyof typeof children])
+        );
+      });
+
+      return {
+        ...element,
+        props: { ...element.props, children: childrenResult },
+      };
+    }
+
+    return {
+      ...element,
+      props: { ...element.props, children: element.props.children },
+    };
+  };
+
+  const fixedElement = convertChildrenAsArray(
+    element as ReactElement<{ children?: ReactNode }>
+  );
+
+  const { type, props } = fixedElement;
+
+  // Create and return the React element
+  return createElement(
+    type ?? 'div',
+    props,
+    ...(props.children as ReactNode[])
+  );
+};
