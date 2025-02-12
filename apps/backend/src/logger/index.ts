4a3c4b3ed06efb5ba49fc1cefadcf7bf0a43fe24
Aymeric PINEAU <ay.pineau@gmail.com>
2025-02-12 01:49:31 +0100

    fix(backend): comment out file transport logging in production environment

--- apps/backend/src/logger/index.ts
+++ apps/backend/src/logger/index.ts
@@ -18,10 +18,10 @@ export const logger = createLogger({
   ],
 });
 
-if (process.env.NODE_ENV === 'production') {
-  logger.add(
-    new transports.File({ filename: 'logs/error.log', level: 'error' })
-  );
-  logger.add(new transports.File({ filename: 'logs/info.log', level: 'info' }));
-  logger.add(new transports.File({ filename: 'logs/combined.log' }));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   logger.add(
+//     new transports.File({ filename: 'logs/error.log', level: 'error' })
+//   );
+//   logger.add(new transports.File({ filename: 'logs/info.log', level: 'info' }));
+//   logger.add(new transports.File({ filename: 'logs/combined.log' }));
+// }
