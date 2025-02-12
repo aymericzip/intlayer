d66658893087054ca934ead99a5f2f5dcebfc8c2
Aymeric PINEAU <ay.pineau@gmail.com>
2025-02-12 23:00:45 +0100

    feat(editor): enhance getIsEditableSection to support MarkdownContent type

--- packages/@intlayer/design-system/src/components/DictionaryFieldEditor/getIsEditableSection.ts
+++ packages/@intlayer/design-system/src/components/DictionaryFieldEditor/getIsEditableSection.ts
@@ -1,9 +1,15 @@
-import { type ContentNode, getNodeChildren } from '@intlayer/core';
+import {
+  type ContentNode,
+  type MarkdownContent,
+  getNodeChildren,
+  NodeType,
+} from '@intlayer/core';
 
 export const getIsEditableSection = (section: ContentNode): boolean => {
   const children = getNodeChildren(section);
 
   return (
+    (children as MarkdownContent).nodeType === NodeType.Markdown ||
     typeof children === 'string' || // String
     typeof children === 'number' || // Number
     typeof children === 'boolean' || // Boolean
