81c560c14546efefdb8d67e7bfcaa915527d00b7
Aymeric PINEAU <ay.pineau@gmail.com>
2025-02-12 01:06:09 +0100

    fix(backend): update logger import path in index.ts

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -50,7 +50,7 @@ import { doubleCsrfProtection } from '@utils/CSRF';
 import { connectDB } from '@utils/mongoDB/connectDB';
 
 // Logger
-import { logger } from '@logger';
+import { logger } from './logger/index';
 
 const app: Express = express();
 
