e97a62fb31532bf6e2f00cf3a2e709d8cd529ea0
Aymeric PINEAU <ay.pineau@gmail.com>
2025-02-21 00:31:42 +0100

    feat(website): integrate Toaster component into AppProvider for enhanced user notifications

--- packages/intlayer-editor/client/src/components/AppProvider.tsx
+++ packages/intlayer-editor/client/src/components/AppProvider.tsx
@@ -1,11 +1,16 @@
 'use client';
 
+import { Toaster } from '@intlayer/design-system';
 import { AsyncStateProvider } from '@intlayer/design-system/hooks';
 import { type FC, type PropsWithChildren } from 'react';
 import { AnimatePresenceProvider } from './AnimatePresenceProvider';
 
 export const AppProvider: FC<PropsWithChildren> = ({ children }) => (
   <AnimatePresenceProvider>
-    <AsyncStateProvider>{children}</AsyncStateProvider>
+    <AsyncStateProvider>
+      <Toaster />
+
+      {children}
+    </AsyncStateProvider>
   </AnimatePresenceProvider>
 );
