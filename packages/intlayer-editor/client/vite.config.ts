19795d56a8bd4fba73ae38256ec76f4e4bfa8906
Aymeric PINEAU <ay.pineau@gmail.com>
2025-03-08 19:52:38 +0100

    fix(intlayer-editor): restore intlayer-editor vite config

--- packages/intlayer-editor/client/vite.config.ts
+++ packages/intlayer-editor/client/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { intlayerPlugin } from 'vite-intlayer';
 import tailwindcss from '@tailwindcss/vite';
-import packageJson from '../package.json' with { type: 'json' };
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -16,20 +15,4 @@ export default defineConfig({
   },
 
   plugins: [react(), intlayerPlugin(), tailwindcss()],
-
-  build: {
-    rollupOptions: {
-      external: [
-        ...Object.keys(packageJson.dependencies),
-        ...Object.keys(packageJson.peerDependencies),
-        ...Object.keys(packageJson.devDependencies),
-        'react/jsx-runtime',
-        'react/jsx-dev-runtime',
-        '@intlayer/config/client',
-        '@intlayer/config/built',
-        'path',
-        'url',
-      ],
-    },
-  },
 });
