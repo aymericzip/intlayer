0f3a50d0ee69f2ca6e0c865214c15d1dc74f34e1
Aymeric PINEAU <ay.pineau@gmail.com>
2025-03-12 23:23:41 +0100

    feat(website): add preconnect and dns-prefetch links for improved performance

--- apps/website/src/layouts/RootHTMLLayout.tsx
+++ apps/website/src/layouts/RootHTMLLayout.tsx
@@ -11,6 +11,23 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const RootHTMLLayout: FC<LocalParams> = ({ children, locale }) => (
   <html lang={locale} dir={getHTMLTextDir(locale)} suppressHydrationWarning>
+    <head>
+      {/* Preconnect and DNS Prefetch for Google Tag Manager */}
+      <link
+        rel="preconnect"
+        href="https://www.googletagmanager.com"
+        crossOrigin=""
+      />
+      <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
+
+      {/* Preconnect and DNS Prefetch for your first-party backend */}
+      <link
+        rel="preconnect"
+        href={process.env.NEXT_PUBLIC_BACKEND_URL}
+        crossOrigin=""
+      />
+      <link rel="dns-prefetch" href={process.env.NEXT_PUBLIC_BACKEND_URL} />
+    </head>
     <body
       className={cn(
         inter.className,
