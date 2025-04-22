92805a3b2175cb583c5c7881ce67873f5f9ac5f3
Aymeric PINEAU <ay.pineau@gmail.com>
2025-04-22 17:58:20 +0200

    fix(website): add migging 404 page at root

---
+++ apps/website/src/app/404/page.tsx
@@ -0,0 +1,5 @@
+import NotFountPage from '../[locale]/(landing)/[...not-found]/page';
+
+export { generateMetadata } from '../[locale]/(landing)/[...not-found]/metadata';
+
+export default NotFountPage;
