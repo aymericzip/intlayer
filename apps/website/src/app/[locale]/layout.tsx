191a344cc2449696a9a72ab092254b8f4013d061
Aymeric PINEAU <ay.pineau@gmail.com>
2025-05-12 01:44:29 +0200

    feat(website): add revalidation interval to layout for improved caching

--- apps/website/src/app/[locale]/layout.tsx
+++ apps/website/src/app/[locale]/layout.tsx
@@ -2,6 +2,8 @@ import type { FC, PropsWithChildren } from 'react';
 
 export { generateMetadata } from './metadata';
 
+export const revalidate = 60; // revalidate at most every 60s
+
 const RootLayout: FC<PropsWithChildren> = ({ children }) => children;
 
 export default RootLayout;
