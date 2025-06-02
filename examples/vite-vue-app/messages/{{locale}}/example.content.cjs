84a3ace0f5c36a44ca42747385f071f67c826f1d
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-02 23:06:37 +0200

    chore(vite-vue-app): remove unused example content file for localization

--- examples/vite-vue-app/messages/{{locale}}/example.content.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('intlayer').Dictionary} **/
-const exampleContent = {
-  key: 'example',
-  locale: 'es',
-  autoFilled: true,
-  content: { new: 'Este es un ejemplo de un nuevo campo--' },
-};
-
-module.exports = exampleContent;
