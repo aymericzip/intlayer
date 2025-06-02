e7f116e6fd1744e5d85333ace294637c01685787
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-02 17:55:31 +0200

    refactor(chokidar): enhance filterTranslationsPlugin to support single locale input

--- packages/@intlayer/chokidar/src/filterDictionaryLocales.ts
+++ packages/@intlayer/chokidar/src/filterDictionaryLocales.ts
@@ -1,23 +1,28 @@
 import type { Locales } from '@intlayer/config/client';
 import {
   deepTransformNode,
-  Dictionary,
   NodeType,
-  Plugins,
+  type Dictionary,
+  type Plugins,
 } from '@intlayer/core';
 
-const filterTranlationsPlugin = (locales: Locales[]): Plugins => ({
+const filterTranlationsPlugin = (locales: Locales[] | Locales): Plugins => ({
   id: 'filter-translations-plugin',
   canHandle: (node) =>
     typeof node === 'object' && node?.nodeType === NodeType.Translation,
   transform: (node, props, deepTransformNode) => {
     const translationMap = node.translation as Record<Locales, string>;
 
-    const filteredTranslationMap = Object.fromEntries(
-      Object.entries(translationMap).filter(([key]) =>
-        locales.includes(key as Locales)
-      )
-    );
+    const localesArray = Array.isArray(locales) ? locales : [locales];
+    const isSingleLocale = localesArray.length === 1;
+
+    const filteredTranslationMap = isSingleLocale
+      ? translationMap[localesArray[0] as Locales]
+      : Object.fromEntries(
+          Object.entries(translationMap).filter(([key]) =>
+            locales.includes(key as Locales)
+          )
+        );
 
     return {
       ...node,
@@ -34,11 +39,10 @@ const filterTranlationsPlugin = (locales: Locales[]): Plugins => ({
 
 export const filterDictionaryLocales = (
   dictionary: Dictionary,
-  locales: Locales[]
-) => {
-  return deepTransformNode(dictionary, {
+  locales: Locales[] | Locales
+) =>
+  deepTransformNode(dictionary, {
     plugins: [filterTranlationsPlugin(locales)],
     dictionaryKey: dictionary.key,
     keyPath: [],
   });
-};
