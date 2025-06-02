f6d093688fb0167f89f45e9ae6f0258f8dd85629
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-02 20:11:34 +0200

    fix(chokidar): restore 'with { type: 'json' }' by security

--- packages/@intlayer/chokidar/src/transpiler/dictionary_to_main/generateDictionaryListContent.ts
+++ packages/@intlayer/chokidar/src/transpiler/dictionary_to_main/generateDictionaryListContent.ts
@@ -23,7 +23,7 @@ export const generateDictionaryListContent = (
   // Import all dictionaries
   dictionariesRef.forEach((dictionary) => {
     if (format === 'esm')
-      content += `import ${dictionary.hash} from '${dictionary.relativePath}' with { type: 'json' };\n`;
+      content += `import ${dictionary.hash} from '${dictionary.relativePath}';\n`;
     if (format === 'cjs')
       content += `const ${dictionary.hash} = require('${dictionary.relativePath}');\n`;
   });
