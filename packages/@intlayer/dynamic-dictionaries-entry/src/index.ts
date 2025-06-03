c47237bb37e5cd48e86738d63fba892fd1809cd5
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-03 14:46:18 +0200

    feat(dynamic-dictionaries-entry): implement dynamic-dictionaries-entry pacakge

---
+++ packages/@intlayer/dynamic-dictionaries-entry/src/index.ts
@@ -0,0 +1,38 @@
+// @ts-nocheck intlayer declared for module augmentation
+
+/**
+ * @intlayer/dynamic-dictionaries-entry is a package that only returns the dynamic dictionary entry file.
+ * Using an external package allow to alias it in the bundle configuration (such as webpack).
+ * The alias allow hot reload the app (such as nextjs) on any dictionary change.
+ */
+
+import { ESMxCJSRequire, getConfiguration, isESModule } from '@intlayer/config';
+import { existsSync } from 'fs';
+import {
+  Dictionary,
+  IntlayerDictionaryTypesConnector,
+  LanguageContent,
+} from 'intlayer';
+import { join } from 'path';
+
+export const getDynamicDictionaries = () => {
+  const { content } = getConfiguration();
+
+  const dictionariesPath = join(
+    content.mainDir,
+    isESModule ? 'dynamic_dictionaries.mjs' : 'dynamic_dictionaries.cjs'
+  );
+  let dictionaries: Record<
+    IntlayerDictionaryTypesConnector['key'],
+    LanguageContent<Dictionary>
+  > = {};
+
+  if (existsSync(dictionariesPath)) {
+    delete ESMxCJSRequire.cache[dictionariesPath];
+    dictionaries = ESMxCJSRequire(dictionariesPath);
+  }
+
+  return dictionaries;
+};
+
+export default (() => getDynamicDictionaries())();
