9bb79d1c1e4ec7373bf17c302c89acbf0242b207
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-03 20:47:27 +0200

    fix(chokidar): correct locale string formatting in dynamic dictionary generation

--- packages/@intlayer/chokidar/src/transpiler/declaration_file_to_dictionary/intlayer_dictionary/writeDynamicDictionary.ts
+++ packages/@intlayer/chokidar/src/transpiler/declaration_file_to_dictionary/intlayer_dictionary/writeDynamicDictionary.ts
@@ -43,10 +43,10 @@ export const generateDictionaryEntryPoint = (
       );
 
       if (format === 'esm') {
-        return `  ${locale}: () => import('./${relativePath}', { assert: { type: 'json' }}).then(mod => mod.default)`;
+        return `  '${locale}': () => import('./${relativePath}', { assert: { type: 'json' }}).then(mod => mod.default)`;
       }
 
-      return `  ${locale}: () => Promise.resolve(require('./${relativePath}'))`;
+      return `  '${locale}': () => Promise.resolve(require('./${relativePath}'))`;
     })
     .join(',\n');
 
