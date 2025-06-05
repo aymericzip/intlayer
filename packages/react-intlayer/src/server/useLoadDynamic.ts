a8d8746ef17f6fecc8efc8afe545c8ee9dab9e03
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-06 01:13:13 +0200

    fix(react-intlayer): update import statements to use React directly for caching and dictionary retrieval

--- packages/react-intlayer/src/server/useLoadDynamic.ts
+++ packages/react-intlayer/src/server/useLoadDynamic.ts
@@ -1,10 +1,11 @@
-import react, { use } from 'react';
+import react from 'react';
 
 export const useLoadDynamic = <T>(_key: string, promise: Promise<T>): T => {
   /** @ts-ignore remove error Property 'cache' does not exist on type 'typeof React'. */
   const cachedPromise = react.cache(async () => await promise)();
 
-  const dictionary = use(cachedPromise);
+  /** @ts-ignore remove error Property 'use' does not exist on type 'typeof React'. */
+  const dictionary = react.use(cachedPromise);
 
   return dictionary;
 };
