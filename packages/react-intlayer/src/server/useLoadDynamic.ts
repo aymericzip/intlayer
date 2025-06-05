fade140b470b699008897c70093d8ed61e1cc704
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-05 15:03:56 +0200

    fix(react-intlayer): simplify cache implementation in useLoadDynamic by removing fallback function

--- packages/react-intlayer/src/server/useLoadDynamic.ts
+++ packages/react-intlayer/src/server/useLoadDynamic.ts
@@ -1,12 +1,8 @@
 import react, { use } from 'react';
 
-const cacheFallback = () => () => ({ value: undefined });
-
 export const useLoadDynamic = <T>(_key: string, promise: Promise<T>): T => {
   /** @ts-ignore remove error Property 'cache' does not exist on type 'typeof React'. */
-  const cache = react.cache ?? cacheFallback;
-
-  const cachedPromise = cache(async () => await promise)();
+  const cachedPromise = react.cache(async () => await promise)();
 
   const dictionary = use(cachedPromise);
 
