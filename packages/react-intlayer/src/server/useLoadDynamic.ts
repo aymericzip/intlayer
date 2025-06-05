65d39945e8736dfdc9c3da78c6a08d7ae8fc3c03
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-05 13:44:56 +0200

    fix(react-intlayer): resolve TypeScript error by implementing cache fallback in useLoadDynamic

--- packages/react-intlayer/src/server/useLoadDynamic.ts
+++ packages/react-intlayer/src/server/useLoadDynamic.ts
@@ -1,7 +1,11 @@
-/** @ts-ignore remove error Property 'cache' does not exist on type 'typeof React'. */
-import { cache, use } from 'react';
+import react, { use } from 'react';
+
+const cacheFallback = () => () => ({ value: undefined });
 
 export const useLoadDynamic = <T>(_key: string, promise: Promise<T>): T => {
+  /** @ts-ignore remove error Property 'cache' does not exist on type 'typeof React'. */
+  const cache = react.cache ?? cacheFallback;
+
   const cachedPromise = cache(async () => await promise)();
 
   const dictionary = use(cachedPromise);
