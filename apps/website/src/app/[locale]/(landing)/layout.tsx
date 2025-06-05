321e3a2de27a8abaf6fd2b1366a8aa5d53691718
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-05 15:20:15 +0200

    refactor(website): remove StatupFrame component from LandingLayout for simplification

--- apps/website/src/app/[locale]/(landing)/layout.tsx
+++ apps/website/src/app/[locale]/(landing)/layout.tsx
@@ -1,35 +1,12 @@
 import { PageLayout } from '@layouts/PageLayout';
 import type { NextLayoutIntlayer } from 'next-intlayer';
-import { FC, PropsWithChildren } from 'react';
-
-const StatupFrame: FC<PropsWithChildren> = () => {
-  return (
-    <a
-      href="https://startupfa.me/s/intlayer.org?utm_source=intlayer.org"
-      target="_blank"
-      className="absolute bottom-5 right-5 opacity-5"
-    >
-      <img
-        src="https://startupfa.me/badges/featured/dark.webp"
-        alt="Featured on Startup Fame"
-        width="171"
-        height="54"
-      />
-    </a>
-  );
-};
 
 const LandingLayout: NextLayoutIntlayer<{
   isSpecial: boolean;
 }> = async ({ children, params }) => {
   const { locale } = await params;
 
-  return (
-    <PageLayout locale={locale}>
-      <StatupFrame />
-      {children}
-    </PageLayout>
-  );
+  return <PageLayout locale={locale}>{children}</PageLayout>;
 };
 
 export default LandingLayout;
