2540275bd1f39aa250fd56142f4823e616fd6ce8
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-09 00:01:05 +0200

    refactor(release): update changeset version script to use async/await and switch to pnpm for lockfile updates

--- .github/changeset-version.js
+++ .github/changeset-version.js
@@ -2,11 +2,29 @@
 // https://github.com/cloudflare/wrangler2/blob/main/.github/changeset-version.js
 
 import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
 
 // This script is used by the `release.yml` workflow to update the version of the packages being released.
-// The standard step is only to run `changeset version` but this does not update the package-lock.json file.
-// So we also run `npm install`, which does this update.
+// The standard step is only to run `changeset version` but this does not update the package-lock.yaml file.
+// So we also run `pnpm install`, which does this update.
 // This is a workaround until this is handled automatically by `changeset version`.
 // See https://github.com/changesets/changesets/issues/421.
-exec('npx changeset version');
-exec('npm install');
+
+const runChangesetVersion = async () => {
+  try {
+    console.log('Running changeset version...');
+    await execAsync('npx changeset version');
+
+    console.log('Updating lockfile with pnpm install...');
+    await execAsync('pnpm install --frozen-lockfile=false');
+
+    console.log('Changeset version completed successfully');
+  } catch (error) {
+    console.error('Error during changeset version:', error);
+    process.exit(1);
+  }
+};
+
+runChangesetVersion();
