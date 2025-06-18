9cd4daeef4dd268510b18fcea8ab0a5488f43b7c
Aymeric PINEAU <ay.pineau@gmail.com>
2025-06-19 00:10:48 +0200

    chore(vue-intlayer): remove 'use client' directive from useDictionaryDynamic and useCrossFrameMessageListener files

--- packages/vue-intlayer/src/client/useDictionaryDynamic.ts
+++ packages/vue-intlayer/src/client/useDictionaryDynamic.ts
@@ -1,5 +1,3 @@
-'use client';
-
 import type { LocalesValues } from '@intlayer/config/client';
 import type {
   Dictionary,
--- packages/vue-intlayer/src/editor/useCrossFrameMessageListener.ts
+++ packages/vue-intlayer/src/editor/useCrossFrameMessageListener.ts
@@ -1,5 +1,3 @@
-'use client';
-
 import { type MessageKey, compareUrls } from '@intlayer/editor';
 import {
   getCurrentInstance,
