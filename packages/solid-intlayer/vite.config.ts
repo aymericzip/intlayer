85bbe336d0f889fe63316ffc580a365b3e72fe9f
Aymeric PINEAU <ay.pineau@gmail.com>
2025-07-02 15:21:40 +0200

    test(solid): remove depreciated solid-testing-library dependency and clean up vite.config.ts test configuration

--- packages/solid-intlayer/vite.config.ts
+++ packages/solid-intlayer/vite.config.ts
@@ -52,11 +52,6 @@ export default defineConfig({
       '.vue', // enable resolving imports like './UI/ContentSelector.vue' :contentReference[oaicite:1]{index=1}:contentReference[oaicite:2]{index=2}
     ],
   },
-  test: {
-    deps: {
-      inline: [/solid-testing-library/],
-    },
-  },
 
   plugins: [
     solidPlugin(),
