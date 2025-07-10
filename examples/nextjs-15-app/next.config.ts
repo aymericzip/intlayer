f4e8814723730583645fb7c8bfc24d1905ebbd3a
Aymeric PINEAU <ay.pineau@gmail.com>
2025-07-10 21:20:55 +0200

    feat(nextjs): add transpilePackages option for @intlayer/design-system

--- examples/nextjs-15-app/next.config.ts
+++ examples/nextjs-15-app/next.config.ts
@@ -6,6 +6,7 @@ const RETURN_BUNDLE_ANALYZER = true;
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['@intlayer/design-system'],
 
   webpack: (config, { isServer }) => {
     if (!isServer) {
