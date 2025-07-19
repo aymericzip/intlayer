88ad689d66fec66d0007d2ab1ed39b8c04c5e511
Aymeric PINEAU <ay.pineau@gmail.com>
2025-07-19 16:20:09 +0200

    fix(website): update Navbar button classes for improved layout and alignment

--- apps/website/src/components/Navbar/index.tsx
+++ apps/website/src/components/Navbar/index.tsx
@@ -119,7 +119,7 @@ export const Navbar: FC = () => {
               color="text"
               label={logoutContent.label.value}
               onClick={handleLogOut}
-              className="!rounded-2xl leading-6"
+              className="!rounded-2xl leading-6 flex"
               size="md"
             >
               {logoutContent.title}
@@ -130,7 +130,7 @@ export const Navbar: FC = () => {
               href={login.url.value}
               variant="button"
               color="text"
-              className="!rounded-2xl leading-6"
+              className="!rounded-2xl leading-6 text-center font-medium"
             >
               {login.title}
             </Link>
