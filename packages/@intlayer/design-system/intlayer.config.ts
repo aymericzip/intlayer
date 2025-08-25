da3b580de89f78a6fa2687372630ea7df4a357fb
Aymeric PINEAU <ay.pineau@gmail.com>
2025-08-25 23:48:04 +0200

    feat(design-system): add initial internationalization configuration with supported locales

---
+++ packages/@intlayer/design-system/intlayer.config.ts
@@ -0,0 +1,35 @@
+import { Locales, type IntlayerConfig } from 'intlayer';
+
+export const locales: Locales[] = [
+  Locales.ENGLISH,
+  Locales.RUSSIAN,
+  Locales.JAPANESE,
+  Locales.FRENCH,
+  Locales.KOREAN,
+  Locales.CHINESE,
+  Locales.SPANISH,
+  Locales.GERMAN,
+  Locales.ARABIC,
+  Locales.ITALIAN,
+  Locales.ENGLISH_UNITED_KINGDOM,
+  Locales.PORTUGUESE,
+  Locales.HINDI,
+];
+export const defaultLocale: Locales = Locales.ENGLISH;
+
+const config: IntlayerConfig = {
+  internationalization: {
+    locales,
+    defaultLocale,
+    strictMode: 'strict',
+  },
+  build: {
+    optimize: false,
+    importMode: 'static',
+  },
+  log: {
+    mode: 'verbose',
+  },
+};
+
+export default config;
