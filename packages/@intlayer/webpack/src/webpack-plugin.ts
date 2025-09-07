ee02a029d50cf19d9c885715b11e2f0c4a1b0b24
Aymeric PINEAU <ay.pineau@gmail.com>
2025-09-07 21:33:05 +0200

    refactor(webpack): remove unused IntlayerEventListener import to clean up code

--- packages/@intlayer/webpack/src/webpack-plugin.ts
+++ packages/@intlayer/webpack/src/webpack-plugin.ts
@@ -1,4 +1,3 @@
-import { IntlayerEventListener } from '@intlayer/api';
 import { prepareIntlayer, watch } from '@intlayer/chokidar';
 import { getConfiguration, logger } from '@intlayer/config';
 import type { Compiler } from 'webpack';
@@ -7,7 +6,6 @@ import type { Compiler } from 'webpack';
 export class IntlayerPlugin {
   private isWatching = false; // Flag to ensure we only start the watcher after the first build
   private configuration = getConfiguration();
-  private eventListener: IntlayerEventListener | undefined;
 
   async apply(compiler: Compiler): Promise<void> {
     if (this.configuration.content.watch) {
