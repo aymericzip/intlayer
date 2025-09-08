7de2cafc0a98f70c26e537c1151fdb10c540c99b
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2025-09-09 01:07:14 +0300

    fix: update prettier config and remove semicolons for consistency
    fix: adjust imports in ls.content.ts to remove semicolons
    
    fix: modify routeTree.gen.ts to remove semicolons and improve readability
    
    chore: comment out type safety registration in router.tsx
    
    fix: update __root.tsx to remove semicolons and improve consistency
    
    fix: enhance locale routing in route.tsx with redirection logic
    
    docs: add comprehensive guide for integrating Intlayer with Tanstack Start

--- examples/tanstack-start-app/src/routes/{-$locale}/route.tsx
+++ examples/tanstack-start-app/src/routes/{-$locale}/route.tsx
@@ -1,22 +1,33 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { createFileRoute, Navigate, Outlet } from '@tanstack/react-router';
 import { configuration } from 'intlayer';
-import { IntlayerProvider } from 'react-intlayer';
+import { IntlayerProvider, useLocale } from 'react-intlayer';
 
 import { useI18nHTMLAttributes } from '@/hooks/useI18nHTMLAttributes';
 
 export const Route = createFileRoute('/{-$locale}')({
-  component: RouteComponent,
+  component: LayoutComponent,
 });
 
-function RouteComponent() {
+function LayoutComponent() {
   useI18nHTMLAttributes();
 
   const { locale } = Route.useParams();
+  const { locale: selectedLocale } = useLocale();
+  const { defaultLocale } = configuration.internationalization;
+  const { prefixDefault } = configuration.middleware;
+
+  // Redirect to the default locale if no locale is present in the URL when prefixDefault is true
+  if (selectedLocale === defaultLocale && !locale && prefixDefault) {
+    return <Navigate replace to={defaultLocale} />;
+  }
+
+  // Redirect to the selected locale if the locale in the URL does not match the selected locale
+  if (selectedLocale !== defaultLocale && !locale) {
+    return <Navigate replace to={selectedLocale} />;
+  }
 
   return (
-    <IntlayerProvider
-      locale={locale ?? configuration.internationalization.defaultLocale}
-    >
+    <IntlayerProvider locale={locale}>
       <Outlet />
     </IntlayerProvider>
   );
