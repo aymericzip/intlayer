bbd449e6c9036aac0ab2ed14d825c0f6f6181c5a
victorbuikem <chibuikemkalu466@gmail.com>
2025-09-22 20:03:03 +0100

    chore: update to test svelte application

---
+++ examples/vite-svelte-app/src/Content.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { useIntLayer, useDictionary } from 'svelte-intlayer';
+  import appDictionary from './app.content';
+
+  let content = useIntLayer('app');
+  let dictionary = useDictionary(appDictionary);
+</script>
+
+<p>{content.title}</p>
+<p>{dictionary.readTheDocs}</p>
