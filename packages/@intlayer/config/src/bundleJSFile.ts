224ed7f51229e70e7c22fddf256f60e4a877abf7
Aymeric PINEAU <ay.pineau@gmail.com>
2025-10-01 21:14:31 +0200

    feat(config): add bundleJSFile function for building JavaScript files and enhance configuration retrieval with plugins support

---
+++ packages/@intlayer/config/src/bundleJSFile.ts
@@ -0,0 +1,30 @@
+import { build, BuildOptions } from 'esbuild';
+
+const commonBuildOptions = {
+  bundle: true,
+  format: 'cjs',
+  platform: 'node',
+  target: 'es2019',
+  sourcemap: false,
+  logLevel: 'silent',
+  write: true,
+  // Bundle relative/local files, but keep bare module imports external
+  packages: 'external',
+  loader: {
+    '.js': 'js',
+    '.jsx': 'jsx',
+    '.mjs': 'js',
+    '.ts': 'ts',
+    '.tsx': 'tsx',
+    '.cjs': 'js',
+    '.json': 'json',
+    '.md': 'text',
+    '.mdx': 'text',
+  },
+} as const;
+
+export const bundleJSFile = async (buildOptions: BuildOptions) =>
+  await build({
+    ...commonBuildOptions,
+    ...buildOptions,
+  });
