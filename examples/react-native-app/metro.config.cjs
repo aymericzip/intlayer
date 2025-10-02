b25cab3f5f0eb1522f65d429a500ba0da6b9a987
Aymeric PINEAU <ay.pineau@gmail.com>
2025-10-02 14:56:16 +0200

    refactor(react-native-app): remove example React Native app and related assets

--- examples/react-native-app/metro.config.cjs
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { configMetroIntlayer } = require('react-native-intlayer/metro');
-
-module.exports = (async () => {
-  const defaultConfig = getDefaultConfig(__dirname);
-
-  return await configMetroIntlayer(defaultConfig);
-})();
