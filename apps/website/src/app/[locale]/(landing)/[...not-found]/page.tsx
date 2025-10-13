3f6ecf660a3ee68c0c9dea34175c4da0de9e1e8a
Aymeric PINEAU <ay.pineau@gmail.com>
2025-10-13 22:58:47 +0200

    fix(website): set default locale for NotFound page and update content rendering

--- apps/website/src/app/[locale]/(landing)/[...not-found]/page.tsx
+++ apps/website/src/app/[locale]/(landing)/[...not-found]/page.tsx
@@ -1,24 +1,35 @@
+import { Locales } from 'intlayer';
 import type { NextPageIntlayer } from 'next-intlayer';
-import { IntlayerServerProvider, useIntlayer } from 'next-intlayer/server';
+import { useIntlayer } from 'next-intlayer/server';
 
 export { generateMetadata } from './metadata';
 
-const NotFountPage: NextPageIntlayer = async ({ params }) => {
+/**
+ * Fallback params for /404 page
+ */
+const defaultParams: Promise<{ locale: Locales }> = new Promise((resolve) =>
+  resolve({
+    locale: Locales.ENGLISH,
+  })
+);
+
+const NotFountPage: NextPageIntlayer = async ({ params = defaultParams }) => {
   const { locale } = await params;
-  const { title, content } = useIntlayer('not-found', locale);
+
+  const content = useIntlayer('not-found', locale);
 
   return (
-    <IntlayerServerProvider locale={locale}>
-      <h1 className="hidden">{title}</h1>
+    <>
+      <h1 className="hidden">{content?.title}</h1>
       <span className="m-32 flex justify-center gap-3 text-center font-bold text-4xl text-darkGray md:justify-end">
         <span className="relative flex items-center">
-          {content}
+          {content?.content}
           <span className="-translate-x-1/2 absolute left-1/2 text-[9rem] opacity-10">
             404
           </span>
         </span>
-      </span>{' '}
-    </IntlayerServerProvider>
+      </span>
+    </>
   );
 };
 
