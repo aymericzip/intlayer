8c88ce01ea9417840ffab053270e46fd75adbe9e
Aymeric PINEAU <ay.pineau@gmail.com>
2025-10-15 07:39:18 +0200

    fix(website): adjust padding class in DocumentationRender component for better responsiveness

--- apps/website/src/components/DocPage/DocumentationRender.tsx
+++ apps/website/src/components/DocPage/DocumentationRender.tsx
@@ -20,7 +20,7 @@ export const DocumentationRender: FC<DocumentationRenderProps> = ({
 
   const isDarkMode = resolvedTheme === 'dark';
   return (
-    <div className="flex flex-col gap-8 p-10 md:px-0">
+    <div className="flex flex-col gap-8 p-10 max-md:px-0">
       <MarkdownRenderer
         isDarkMode={isDarkMode}
         locale={locale}
