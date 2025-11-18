565cd0869e2aa49b82b8ec2140c9ce96df05aa14
Aymeric PINEAU <ay.pineau@gmail.com>
2025-11-18 20:55:56 +0100

    fix(website): add storageKey to NextThemesProvider for improved iframe compatibility

--- apps/website/src/providers/ThemeProvider.tsx
+++ apps/website/src/providers/ThemeProvider.tsx
@@ -7,4 +7,11 @@ import type { FC, PropsWithChildren } from 'react';
 export const ThemeProvider: FC<PropsWithChildren<ThemeProviderProps>> = ({
   children,
   ...props
-}) => <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}) => (
+  <NextThemesProvider
+    storageKey="intlayer-theme" // Limit issue with children app in iframe
+    {...props}
+  >
+    {children}
+  </NextThemesProvider>
+);
