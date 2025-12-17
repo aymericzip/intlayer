bf75196a9b2ec980a7acca66282e320e057f951b
Aymeric PINEAU <ay.pineau@gmail.com>
2025-12-17 03:03:24 +0100

    chore(website): update Cache-Control header to extend max-age for improved caching

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -118,7 +118,7 @@ const secureHeaders = {
 const globalHeaders = [
   {
     key: 'Cache-Control',
-    value: 'public, max-age=60, stale-while-revalidate=30',
+    value: 'public, max-age=31536000, stale-while-revalidate=30',
   },
   { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
   { key: 'Cross-Origin-Embedder-Policy', value: 'same-origin' },
