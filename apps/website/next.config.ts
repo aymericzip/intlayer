ce3d9bee2e94588e412ee7f7f1da2ae1043b0688
Aymeric PINEAU <ay.pineau@gmail.com>
2025-12-18 01:06:15 +0100

    refactor(website): remove unused YouTube image host configurations from Next.js config

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -195,14 +195,6 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
       },
-      {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'yt3.ggpht.com',
-      },
     ],
   },
   webpack: (config) => {
