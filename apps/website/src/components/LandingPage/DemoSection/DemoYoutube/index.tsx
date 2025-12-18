bb1283911e35381a63451d9327f3bbee1459b79c
Aymeric PINEAU <ay.pineau@gmail.com>
2025-12-18 01:03:51 +0100

    refactor(website): simplify YouTube embed URL in DemoYoutube component by removing unnecessary origin parameter

--- apps/website/src/components/LandingPage/DemoSection/DemoYoutube/index.tsx
+++ apps/website/src/components/LandingPage/DemoSection/DemoYoutube/index.tsx
@@ -1,21 +1,19 @@
-import { useIntlayer } from 'next-intlayer/server';
+import { useIntlayer } from 'next-intlayer';
 import type { FC } from 'react';
 
 const videoIdEN = 'e_PPG7PTqGU';
 
 export const DemoYoutube: FC = () => {
-  const content = useIntlayer('demo-youtube');
+  const { howToInternationalize } = useIntlayer('demo-youtube');
 
   return (
     <div className="m-auto aspect-16/9 w-full overflow-hidden rounded-lg border-0">
       <iframe
-        title={content.demoYoutubeHowToInternationalize.value}
+        title={howToInternationalize.value}
         className="h-full w-full"
         allow="autoplay; gyroscope;"
         loading="lazy"
-        src={`https://www.youtube.com/embed/${videoIdEN}?autoplay=1&origin=${encodeURIComponent(
-          window.location.origin
-        )}&controls=0&rel=1`}
+        src={`https://www.youtube.com/embed/${videoIdEN}?autoplay=1&controls=0&rel=1`}
       />
     </div>
   );
