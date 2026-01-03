28519b3c3579e975a06b5f268951477860262be8
Aymeric PINEAU <ay.pineau@gmail.com>
2026-01-03 20:47:30 +0100

    feat(app): enhance LandingLayout to include a dynamic DashboardFooter with formatted footer links

--- apps/app/src/app/[locale]/(other)/layout.tsx
+++ apps/app/src/app/[locale]/(other)/layout.tsx
@@ -1,12 +1,34 @@
+import { DashboardFooter } from '@components/Dashboard/DashboardFooter';
 import { PageLayout } from '@layouts/PageLayout';
 import type { NextLayoutIntlayer } from 'next-intlayer';
+import { useIntlayer } from 'next-intlayer/server';
 
 export { generateStaticParams } from 'next-intlayer';
 
 const LandingLayout: NextLayoutIntlayer = async ({ children, params }) => {
   const { locale } = await params;
+  const { footerLinks } = useIntlayer('dashboard-navbar-content');
 
-  return <PageLayout locale={locale}>{children}</PageLayout>;
+  const formattedFooterLinks = footerLinks.map(
+    (el: {
+      href: { value: string };
+      label: { value: string };
+      text: { value: string };
+    }) => ({
+      href: el.href.value,
+      label: el.label.value,
+      text: el.text.value,
+    })
+  );
+
+  return (
+    <PageLayout
+      footer={<DashboardFooter locale={locale} links={formattedFooterLinks} />}
+      locale={locale}
+    >
+      {children}
+    </PageLayout>
+  );
 };
 
 export default LandingLayout;
