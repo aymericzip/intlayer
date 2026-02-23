235bdd9ca6915ffb463081df7851b5d9922d7280
Aymeric PINEAU <ay.pineau@gmail.com>
2026-02-23 11:37:31 -0600

    refactor(chokidar): reexport missing exports

--- packages/@intlayer/chokidar/src/cli.ts
+++ packages/@intlayer/chokidar/src/cli.ts
@@ -1,5 +1,7 @@
+export * from './getContentDeclarationFileTemplate';
 export * from './init/index';
 export * from './installSkills/index';
+export * from './listDictionariesPath';
 export * from './listGitFiles';
 export * from './listProjects';
 export * from './transformFiles/index';
