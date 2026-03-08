e35c05a08ce85486fe3c8698ac356641b47276a2
Aymeric PINEAU <ay.pineau@gmail.com>
2026-03-07 18:20:15 -0600

    fix(showcase): fix env var in vite.config

--- apps/showcase/vite.config.ts
+++ apps/showcase/vite.config.ts
@@ -187,7 +187,7 @@ export default defineConfig(({ mode }) => {
         },
         sitemap: {
           enabled: true,
-          host: import.meta.env.VITE_APP_URL,
+          host: 'https://showcase.intlayer.org',
         },
         prerender: {
           enabled: true,
