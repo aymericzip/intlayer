d500c53520ac5ed4f8de2971195a450bd4858617
Aymeric PINEAU <ay.pineau@gmail.com>
2026-03-20 16:47:08 -0600

    fix(app): fix profile dropdown

--- apps/app/src/components/ProfileDropdown/ProfileDropdown.tsx
+++ apps/app/src/components/ProfileDropdown/ProfileDropdown.tsx
@@ -1,22 +1,65 @@
+'use client';
+
+import { Avatar, Button, Container, DropDown } from '@intlayer/design-system';
 import { useUser } from '@intlayer/design-system/hooks';
 import type { FC } from 'react';
-import { ProfilePanel } from '../Auth/ProfilePanel';
-import { ProfileDropDown as ProfileDropDownUI } from './ProfileDropDown/index';
+
+const DROPDOWN_IDENTIFIER = 'profile';
 
 export const ProfileDropDown: FC = () => {
-  const { isAuthenticated, user } = useUser();
+  const { isAuthenticated, user, logout } = useUser();
+
+  if (!isAuthenticated) return null;
 
   const userName = user?.name ?? user?.email ?? '';
 
   return (
-    isAuthenticated && (
-      <ProfileDropDownUI
-        fullname={userName}
-        isLoggedIn={isAuthenticated}
-        src={user?.image ?? undefined}
+    <DropDown identifier={DROPDOWN_IDENTIFIER}>
+      <DropDown.Trigger
+        identifier={DROPDOWN_IDENTIFIER}
+        size="icon-sm"
+        variant="outline"
+        color="text"
+        roundedSize="full"
+        className="border-none p-0!"
       >
-        <ProfilePanel />
-      </ProfileDropDownUI>
-    )
+        <Avatar
+          fullname={userName}
+          isLoggedIn={isAuthenticated}
+          src={user?.image ?? undefined}
+        />
+      </DropDown.Trigger>
+      <DropDown.Panel
+        identifier={DROPDOWN_IDENTIFIER}
+        isFocusable
+        isOverable
+        align="end"
+      >
+        <Container
+          className="min-w-[100px] border border-text/10 p-6"
+          transparency="xs"
+          roundedSize="xl"
+        >
+          <div className="flex flex-col gap-5">
+            {user?.name && (
+              <span className="whitespace-nowrap font-bold text-lg">
+                {user.name}
+              </span>
+            )}
+            {user?.email && (
+              <span className="whitespace-nowrap text-base">{user.email}</span>
+            )}
+            <Button
+              variant="outline"
+              color="text"
+              onClick={logout}
+              label="Logout from showcase"
+            >
+              Logout
+            </Button>
+          </div>
+        </Container>
+      </DropDown.Panel>
+    </DropDown>
   );
 };
