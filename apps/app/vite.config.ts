3b63fd0e892d9cd7bc8f3a8c43786495fb8cdbf7
aymericzip <ay.pineau@gmail.com>
2026-06-04 19:50:33 -0600

    fix(app): disable prerendering to make the build pass

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -222,7 +222,7 @@ export default defineConfig(({ mode }) => {
           host: 'https://app.intlayer.org',
         },
         prerender: {
-          enabled: true,
+          enabled: false,
           crawlLinks: false,
           concurrency: 10,
         },
