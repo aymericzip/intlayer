ced1f3a8d8834b169e416424d5cd9c904319ef37
aymericzip <ay.pineau@gmail.com>
2026-06-12 23:40:31 -0600

    chore(website): remove unused google tag manager

--- apps/website/src/providers/AppProviders.tsx
+++ apps/website/src/providers/AppProviders.tsx
@@ -2,7 +2,7 @@ import { ChunkErrorListener } from '@components/ChunkErrorListener';
 import { ServiceWorkerSubscriber } from '@components/ServiceWorker/ServiceWorkerSubscriber';
 import { ReactQueryProvider } from '@intlayer/design-system/providers';
 import { Toaster } from '@intlayer/design-system/toaster';
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import Script from 'next/script';
 import type { FC, PropsWithChildren } from 'react';
 import { AnimatePresenceProvider } from './AnimatePresenceProvider';
@@ -15,11 +15,7 @@ export const AppProviders: FC<PropsWithChildren> = ({ children }) => {
       <ServiceWorkerSubscriber />
       <ReactQueryProvider>
         <Toaster />
-        {process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID && (
-          <GoogleTagManager
-            gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}
-          />
-        )}
+
         {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && (
           <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
         )}
