55a9d9f1d0d74efaface71853e81471ac46be37e
aymericzip <ay.pineau@gmail.com>
2026-06-23 17:33:50 -0600

    chore: replace clsx and tailwind-merge with cnfast utility in design system

--- packages/@intlayer/design-system/src/utils/cn.ts
+++ packages/@intlayer/design-system/src/utils/cn.ts
@@ -1,4 +1,1 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+export { cn } from 'cnfast';
