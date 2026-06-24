5e47dacc51f098ed2b334b68393ea73ce63f1a5f
aymericzip <ay.pineau@gmail.com>
2026-06-23 23:16:43 -0600

    fix(compat): fix cjs output for nextjs compat

--- compat/next-intl/tsdown.config.ts
+++ compat/next-intl/tsdown.config.ts
@@ -1,6 +1,4 @@
 import { options } from '@utils/tsdown-config';
-import { defineConfig, type UserConfig } from 'tsdown';
+import { defineConfig } from 'tsdown';
 
-const [esm, , types] = options;
-
-export default defineConfig([esm, types] as UserConfig[]);
+export default defineConfig(options);
