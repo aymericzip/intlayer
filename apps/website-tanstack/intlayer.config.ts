f58ac33e562583d5e379ed7a6be1bc040ac637d7
aymericzip <ay.pineau@gmail.com>
2026-08-10 16:49:53 -0600

    refactor(website-tanstack): enhance error handling and reload logic for dynamic imports

--- apps/website-tanstack/intlayer.config.ts
+++ apps/website-tanstack/intlayer.config.ts
@@ -5,23 +5,23 @@ import { nextjsRewrite } from 'intlayer/routing';
 
 export const locales: Locale[] = [
   Locales.ENGLISH,
-  Locales.RUSSIAN,
-  Locales.JAPANESE,
+  // Locales.RUSSIAN,
+  // Locales.JAPANESE,
   Locales.FRENCH,
-  Locales.KOREAN,
-  Locales.CHINESE,
-  Locales.SPANISH,
-  Locales.GERMAN,
-  Locales.ARABIC,
-  Locales.ITALIAN,
-  Locales.ENGLISH_UNITED_KINGDOM,
-  Locales.PORTUGUESE,
-  Locales.HINDI,
-  Locales.TURKISH,
-  Locales.POLISH,
-  Locales.INDONESIAN,
-  Locales.VIETNAMESE,
-  Locales.UKRAINIAN,
+  // Locales.KOREAN,
+  // Locales.CHINESE,
+  // Locales.SPANISH,
+  // Locales.GERMAN,
+  // Locales.ARABIC,
+  // Locales.ITALIAN,
+  // Locales.ENGLISH_UNITED_KINGDOM,
+  // Locales.PORTUGUESE,
+  // Locales.HINDI,
+  // Locales.TURKISH,
+  // Locales.POLISH,
+  // Locales.INDONESIAN,
+  // Locales.VIETNAMESE,
+  // Locales.UKRAINIAN,
 ];
 export const defaultLocale = Locales.ENGLISH;
 
@@ -83,6 +83,11 @@ const config: CustomIntlayerConfig = {
   },
   compiler: {
     output: ({ fileName }) => `./${fileName}.content.ts`,
+  },
+  build: {
+    optimize: true,
+    minify: true,
+    purge: true,
   },
   editor: {
     enabled: false,
