6e399122f317879473ad9f864b1b26652608ed67
Tornado Softwares <tornadogamesfrance@gmail.com>
2026-08-19 01:00:45 +0200

    chore: main landing switcher

--- apps/website-tanstack/src/components/LocaleSwitcher/LocaleSwitcher.tsx
+++ apps/website-tanstack/src/components/LocaleSwitcher/LocaleSwitcher.tsx
@@ -61,7 +61,7 @@ export const LocaleSwitcher: FC<LocaleSwitcherProps> = ({
           size="sm"
           className="p-0!"
           variant="outline"
-          color="text"
+          color="foreground"
           roundedSize="5xl"
           onClick={handleFocusInput}
         >
