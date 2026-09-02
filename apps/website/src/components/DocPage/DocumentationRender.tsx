a0fc1586f06f85835727f862eaabe806c55f474b
aymericzip <ay.pineau@gmail.com>
2026-09-01 18:30:55 -0600

    fix(website): remove duplicate Accordion components from DocumentationRender

--- apps/website/src/components/DocPage/DocumentationRender.tsx
+++ apps/website/src/components/DocPage/DocumentationRender.tsx
@@ -23,35 +23,6 @@ const I18nBenchmark = lazy(() =>
   }))
 );
 
-const Accordion = ({ children, ...props }: AccordionProps) => (
-  <AccordionBase
-    {...props}
-    headerClassName="text-lg!"
-    contentClassName="divide-y divide-neutral"
-  >
-    <div className="mb-8 flex flex-col gap-6 px-4 pt-6 text-sm text-text/80">
-      {children}
-    </div>
-  </AccordionBase>
-);
-
-const AccordionGroup = ({
-  children,
-  ...props
-}: HTMLAttributes<HTMLDivElement>) => (
-  <Container
-    padding="sm"
-    roundedSize="2xl"
-    background="none"
-    border
-    borderColor="neutral"
-    className="flex flex-col gap-1 overflow-hidden"
-    {...props}
-  >
-    {children}
-  </Container>
-);
-
 type DocumentationRenderProps = {
   children: string | ParsedMarkdown;
   /**
