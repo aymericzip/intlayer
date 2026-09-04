535c152aec162eb942d09a2b627fc3a110f57b47
aymericzip <ay.pineau@gmail.com>
2026-09-04 12:06:48 -0600

    chore(astro-react-app): remove unused app

--- examples/astro-react-app/src/components/MultilingualCard.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC } from 'react';
-import { useLocale } from 'react-intlayer';
-
-export const MultilingualCard: FC = () => {
-  const { locale } = useLocale();
-  return <div className="m-auto">Current locale: {locale}</div>;
-};
