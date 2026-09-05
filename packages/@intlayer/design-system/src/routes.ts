921468d4ffbc0ed3764627a6c9feca5dd26296b4
aymericzip <ay.pineau@gmail.com>
2026-09-04 22:30:44 -0600

    fix(design-system): correct default value for Website_Domain

--- packages/@intlayer/design-system/src/routes.ts
+++ packages/@intlayer/design-system/src/routes.ts
@@ -4,7 +4,7 @@
 export const App_Domain = ((import.meta as any).env.VITE_CMS_URL ??
   'app.intlayer.org') as 'app.intlayer.org';
 export const Website_Domain = ((import.meta as any).env.VITE_WEBSITE_URL ??
-  'intlayer.cn') as 'intlayer.org';
+  'intlayer.org') as 'intlayer.org';
 export const Showcase_Domain = 'showcase.intlayer.org' as const;
 export const Backend_Domain = ((import.meta as any).env.VITE_BACKEND_URL ??
   'back.intlayer.org') as 'back.intlayer.org';
